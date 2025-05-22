3886a39aa5f7a44d8a65f03663a68199c8e91594
Sai Ritesh Domakuntla <sairiteshdomakuntla@gmail.com>
2025-05-22 16:17:09 +0530

    add basic pages and routing

---
+++ src/pages/Index.tsx
@@ -0,0 +1,12 @@
+
+import BlinkTacToe from "@/components/BlinkTacToe";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-game-bg">
+      <BlinkTacToe />
+    </div>
+  );
+};
+
+export default Index;
